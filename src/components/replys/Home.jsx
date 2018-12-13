49665174d0aa00ad597e775a0e42307e934b87b8
nwilliamfeng@163.com <nwilliamfeng@163.com>
2018-12-13 20:02:17 +0800

    commit 181213d

--- src/components/replys/Home.jsx
+++ src/components/replys/Home.jsx
@@ -2,31 +2,49 @@ import React, { Component } from 'react'
 
 import { commentActions } from '../../actions'
 import { connect } from 'react-redux';
-import {Pages} from '../../constants'
+import { Pages } from '../../constants'
 import styled from 'styled-components'
- 
- 
-import {ReplyList} from './ReplyList'
 
-import {CommentList} from './CommentList'
 
+import { ReplyList } from './ReplyList'
+
+import { CommentList } from './CommentList'
+
+import { withCookies, Cookies } from 'react-cookie';
 
 class Home extends Component {
 
-   
-    componentDidMount(){
-        const {dispatch} =this.props;
+
+    componentDidMount() {
+        const { dispatch } = this.props;
+        const { cookies } = this.props;
+    //    cookies.set('name', 'name', { path: 'abc' });
+    //https://github.com/reactivestack/cookies/blob/cb26b0caa101dc4760812ff61697deaf1c700ada/packages/universal-cookie/README.md
+           console.log( cookies.get('ct')  )
+     
         dispatch(commentActions.directToCommentPage(Pages.COMMENT));
     }
-    
+
+
+    getCookie = param => {
+        if (document.cookie.length > 0) {
+            let cStart = document.cookie.indexOf(param + "=");
+            if (cStart != -1) {
+                cStart = cStart + param.length + 1;
+                let cEnd = document.cookie.indexOf(";", cStart);
+                if (cEnd == -1) cEnd = document.cookie.length;
+                return decodeURI(document.cookie.substring(cStart, cEnd));
+            }
+        }
+    }
 
 
     render() {
-        const {page} =this.props;
-       return <React.Fragment>
-           {page===Pages.REPLY && <ReplyList/>}
-          <CommentList/>
-       </React.Fragment>
+        const { page } = this.props;
+        return <React.Fragment>
+            {page === Pages.REPLY && <ReplyList />}
+            <CommentList />
+        </React.Fragment>
     }
 }
 
@@ -36,6 +54,6 @@ function mapStateToProps(state) {
 }
 
 
-const container = connect(mapStateToProps)(Home)
+const container = connect(mapStateToProps)(withCookies(Home))
 
 export { container as Home }
